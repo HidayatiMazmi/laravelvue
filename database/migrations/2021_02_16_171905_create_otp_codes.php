7d5af8a4134d02f2684cfaae15b05d4894c2fe03
hidayatimazmi <hidayatimazmi@gmail.com>
2021-02-17 00:47:01 +0700

    uuid_table

---
+++ database/migrations/2021_02_16_171905_create_otp_codes.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOtpCodes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('otp_codes', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('code');
+            $table->datetime('code_date');
+            $table->string('status_code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('otp_codes');
+    }
+}
